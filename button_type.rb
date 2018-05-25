f38ea74f9a33fc57fb1ba584b22d75502625d973
Sanosuke94 <cedric-valcy@hotmail.fr>
2018-05-25 20:47:59 +0200

    Ajout des label et des bouttons

---
+++ button_type.rb
@@ -0,0 +1,9 @@
+class Button
+	
+	def initialize
+		super
+		
+		button_style = Gtk::Button.new
+	end
+	
+end
--- main.rb
+++ main.rb
@@ -10,12 +10,35 @@ class WindowApp < Gtk::Window
 		signal_connect "destroy" do
 			Gtk.main_quit
 		end
+		position
 		
 		set_title 'Interface personnel'
-		set_default_size 600, 400
+		set_default_size 640, 480
 		set_window_position :center
-		show
+		show_all
 	end
+	
+	def position
+		pos = Gtk::Fixed.new
+		pos.put login, 230, 200
+		pos.put password, 230, 250
+		pos.put button, 275, 300
+		add pos
+	end
+	
+	def login
+		login_entry = Gtk::Entry.new
+	end
+	
+	def password
+		password_entry = Gtk::Entry.new
+	end
+	
+	def button
+		submit_button = Gtk::Button.new(:label => "Submit")
+		submit_button.set_size_request 100, 50
+	end
+	
 	
 	
 end
