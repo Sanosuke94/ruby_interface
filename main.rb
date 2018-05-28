6b14f1df4b4ea614d59c5d315c48b36b3524516a
Sanosuke94 <cedric-valcy@hotmail.fr>
2018-05-28 18:56:05 +0200

    suppression du fichier button_type, ajout des images

--- main.rb
+++ main.rb
@@ -7,40 +7,40 @@ class WindowApp < Gtk::Window
 	
 	def initialize
 		super
-		signal_connect "destroy" do
-			Gtk.main_quit
-		end
-		position
+		
+		form
 		
 		set_title 'Interface personnel'
 		set_default_size 640, 480
 		set_window_position :center
 		show_all
+		signal_connect "destroy" do
+			Gtk.main_quit
+		end
 	end
 	
-	def position
+	def form
+		login_entry = Gtk::Entry.new
+		password_entry = Gtk::Entry.new
+		
+		submit_button = Gtk::Button.new(:label => "Submit")
+		submit_button.set_size_request 120, 30
+		
+		login_title = Gtk::Label.new('Login :')
+		password_title = Gtk::Label.new('Password :')
+		
+		pixbuf = GdkPixbuf::Pixbuf.new(:file => 'assets/logo.png', :width => 800, :height => 150)
+		logo = Gtk::Image.new( pixbuf)
+		
 		pos = Gtk::Fixed.new
-		pos.put login, 230, 200
-		pos.put password, 230, 250
-		pos.put button, 275, 300
+		pos.put login_entry, 240, 200
+		pos.put password_entry, 240, 250
+		pos.put submit_button, 275, 300
+		pos.put login_title, 180, 205
+		pos.put password_title, 155, 255
+		pos.put logo, 260, 20
 		add pos
 	end
-	
-	def login
-		login_entry = Gtk::Entry.new
-	end
-	
-	def password
-		password_entry = Gtk::Entry.new
-	end
-	
-	def button
-		submit_button = Gtk::Button.new(:label => "Submit")
-		submit_button.set_size_request 100, 50
-	end
-	
-	
-	
 end
 
 window = WindowApp.new
