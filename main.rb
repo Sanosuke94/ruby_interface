971a516e8f6ace8789ff85a3f7d1c0869054693e
Sanosuke94 <cedric-valcy@hotmail.fr>
2018-05-29 16:26:50 +0200

    recuperation des champs, création nouvel page

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@
 # Première interface graphique
 # 20 mai 2018
 require 'gtk3'
+require_relative 'home'
 
 class WindowApp < Gtk::Window
 	
 	def initialize
 		super
-		
 		form
 		
 		set_title 'Interface personnel'
@@ -17,29 +17,40 @@ class WindowApp < Gtk::Window
 		signal_connect "destroy" do
 			Gtk.main_quit
 		end
+		
 	end
 	
 	def form
-		login_entry = Gtk::Entry.new
-		password_entry = Gtk::Entry.new
+		@login_entry = Gtk::Entry.new
+		@password_entry = Gtk::Entry.new
 		
 		submit_button = Gtk::Button.new(:label => "Submit")
 		submit_button.set_size_request 120, 30
+		
+		submit_button.signal_connect "clicked" do
+			validate
+		end
 		
 		login_title = Gtk::Label.new('Login :')
 		password_title = Gtk::Label.new('Password :')
 		
 		pixbuf = GdkPixbuf::Pixbuf.new(:file => 'assets/logo.png', :width => 800, :height => 150)
-		logo = Gtk::Image.new( pixbuf)
+		logo = Gtk::Image.new(:pixbuf => pixbuf)
 		
 		pos = Gtk::Fixed.new
-		pos.put login_entry, 240, 200
-		pos.put password_entry, 240, 250
-		pos.put submit_button, 275, 300
+		pos.put @login_entry, 240, 200
+		pos.put @password_entry, 240, 250
+		pos.put submit_button, 267, 300
 		pos.put login_title, 180, 205
 		pos.put password_title, 155, 255
 		pos.put logo, 260, 20
 		add pos
+
+	end
+	
+	def validate
+		puts @login_entry.text
+		puts @password_entry.text
 	end
 end
 
